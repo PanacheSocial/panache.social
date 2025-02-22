37b9e7f9d26642cb82fad9175cbe0b34fbdaf92c
yassine20011 <medalgamer47@gmail.com>
2025-02-22 21:48:04 +0100

    feat: implement reset password form with validation and loading state

--- app/auth/ui/components/reset_password_form.tsx
+++ app/auth/ui/components/reset_password_form.tsx
@@ -11,36 +11,70 @@ import {
 import { Input } from '#common/ui/components/input'
 import { Label } from '#common/ui/components/label'
 import useTranslate from '#common/ui/hooks/use_translate'
+import { useForm, usePage } from '@inertiajs/react'
+
+type inertiaProps = {
+  email: string
+  signature: string
+}
 
 export function ResetPasswordForm({ className, ...props }: React.ComponentPropsWithoutRef<'div'>) {
   const t = useTranslate('auth')
+  const { setData, post, processing, errors } = useForm({
+    password: '',
+    confirmationPassword: '',
+  })
+  const { email, signature } = usePage<inertiaProps>().props
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault()
+    post(`/auth/reset_password/${email}?signature=${signature}`)
+  }
 
   return (
     <div className={cn('flex flex-col gap-6', className)} {...props}>
       <Card>
         <CardHeader className="text-center">
           <CardTitle className="text-4xl font-normal font-serif">
-            {t('forgot_password_title')}
+            {t('reset_password_title')}
           </CardTitle>
-          <CardDescription>{t('forgot_password_description')}</CardDescription>
+          <CardDescription>{t('reset_password_description')}
+          </CardDescription>
         </CardHeader>
         <CardContent>
-          <form>
+          <form onSubmit={handleSubmit}>
             <div className="grid gap-5">
               <div className="grid gap-2">
-                <Label htmlFor="email">{t('email_label')}</Label>
+                <Label htmlFor="password">{t('password_label')}</Label>
                 <Input
-                  autoComplete="panache-email"
-                  id="email"
-                  name="email"
-                  type="text"
-                  placeholder={t('email_placeholder')}
+                  id="password"
+                  name="password"
+                  type="password"
+                  placeholder='********'
                   required
                   className="pr-20"
+                  onChange={(e) => setData('password', e.target.value)}
                 />
               </div>
-              <Button type="submit" className="!w-full">
-                {t('send_reset_link')}
+              <div className="grid gap-2">
+                <Label htmlFor="confirmationPassword">
+                  {t('confirmation_password_label')}
+                </Label>
+                <Input
+                  id="confirmationPassword"
+                  name="confirmationPassword"
+                  type="password"
+                  placeholder='********'
+                  required
+                  className="pr-20"
+                  onChange={(e) => setData('confirmationPassword', e.target.value)}
+                />
+                {errors.password && (
+                  <p className="text-red-500 text-sm">{errors.password}</p> // TODO: Add translation
+                )}
+              </div>
+              <Button type="submit" className="!w-full" disabled={processing}>
+                {t('change_password')}
               </Button>
             </div>
           </form>
