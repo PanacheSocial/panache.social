6d864c613f41c3d16232c56ee966b1c2422a7652
yassine20011 <medalgamer47@gmail.com>
2025-02-22 21:48:21 +0100

    feat: update reset password notification to include signed URL expiration and render email content

--- app/auth/notifications/reset_password_notification.ts
+++ app/auth/notifications/reset_password_notification.ts
@@ -1,33 +1,26 @@
 import env from '#start/env'
 import { BaseMail } from '@adonisjs/mail'
-// import router from '@adonisjs/core/services/router'
 import User from '#common/models/user'
+import ResetPasswordEmail from '#auth/emails/reset_password_email'
+import router from '@adonisjs/core/services/router'
+import { renderToString } from 'react-dom/server'
 
 export default class ResetPasswordNotification extends BaseMail {
-  from = env.get('EMAIL_FROM')
+  from = `Panache <${env.get('MAIL_FROM_ADDRESS')}>`
   subject = `Reset your password for Panache`
 
   constructor(private user: User) {
     super()
   }
-
-  /**
-   * The "prepare" method is called automatically when
-   * the email is sent or queued.
-   */
   async prepare() {
-    /**
-     * Generate a signed URL with the user's email,
-     * which can be used to reset the password.
-     */
-    // const signedUrl = router.makeSignedUrl(
-    //   'auth.reset_password.show',
-    //   { email: this.user.email },
-    //   { expiresIn: '30m', prefixUrl: env.get('APP_URL'), purpose: 'reset_password' }
-    // )
 
+    const signedUrl = router.makeSignedUrl(
+      'auth.reset_password.show',
+      { email: this.user.email },
+      { expiresIn: '15m', prefixUrl: env.get('APP_URL'), purpose: 'reset_password' }
+    )
     this.message.to(this.user.email)
-
-    this.message.html('emailHtml')
+    const html = renderToString(ResetPasswordEmail({ user: this.user, signedUrl }))
+    this.message.html(html)
   }
 }
